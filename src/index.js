3f4ec58e07c52a35cf8627cacf8c2745ce0c7b00
Nirav-Printf <nirav.thummar@egrambook.com>
2026-05-21 18:59:42 +0530

    refactor server startup logging and import app module in index.js; clean up user model by removing unnecessary whitespace

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
+import { app } from "./app.js";
 
 import connectDB from "./db/index.js";
 
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`server is running on post  : ${process.env.PORT}`);
+      console.log(`server is running on port  : ${process.env.PORT}`);
     });
   })
   .catch((err) => console.log("mongo DB Connecttion error !!!", err));
